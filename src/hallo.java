18a854bb8729f366268e05fcc49d9018a3b778c4
Felix Leiter <felix.leiter00@gmail.com>
2021-11-30 11:29:57 +0100

    Zeile 5 geändert nochmal

--- src/hallo.java
+++ src/hallo.java
@@ -2,7 +2,7 @@ public class hallo {
     public static void main(String[] args){
         System.out.println("Hallo");
         System.out.println("Test!!!!");
-        System.out.println("Zeile 5 geändert");
+        System.out.println("Zeile 5 ");
         System.out.println("Tschüss");
     }
 
