976a8a916b44d181a4d9b089caf0c76ccd8ed8b8
Felix Leiter <felix.leiter00@gmail.com>
2021-11-30 11:22:39 +0100

    Print "test"

--- src/hallo.java
+++ src/hallo.java
@@ -1,6 +1,8 @@
+import java.sql.SQLOutput;
+
 public class hallo {
-    public static void main(String[] args){
-        System.out.println("Hallo");
+    public static void main(String[] args){ System.out.println("Hallo");
+        System.out.println("Test!!!!");
     }
 
 }
